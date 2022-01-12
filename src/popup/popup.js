8df1816f4c6e7cd15fa2b7f9ad1a16e2a21b4e56
Aaron Didner <adidner@gmail.com>
2022-01-11 23:07:53 -0500

    inject and sync logic

---
+++ src/popup/popup.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    function fillInputs(){
+        let pageInputs = document.getElementsByTagName("input");
+        chrome.storage.sync.get("fillValue", ({ fillValue }) => {
+            Array.from(pageInputs).forEach((input) => {
+                input.value = fillValue;
+            });
+        });
+    }
+
+    async function callInjectFillInputs(){
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: fillInputs,
+        });
+    }
+
+    
+
+
+    document.getElementById("fill-button").addEventListener("click", callInjectFillInputs);
+    document.getElementById("fill-input").addEventListener("change", () => {
+        chrome.storage.sync.set({ fillValue: document.getElementById("fill-input").value });
+    })
+});
